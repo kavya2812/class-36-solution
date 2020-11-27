b4b896db20b1578725687a41f5615d1ecc46034e
kavya2812 <71318063+kavya2812@users.noreply.github.com>
2020-11-27 23:26:11 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,34 @@
+class Form{
+    constructor()
+    {
+
+    }
+
+    display()
+    {
+        var title=createElement('h2');
+        title.html("Car Racing Game");
+        title.position(130,0);
+
+        var input= createInput("Name");
+        input.position(500,160);
+
+        var button= createButton('Play');
+        button.position(500,200);
+
+        var greeting = createElement('h3');
+
+        button.mousePressed(function(){
+            input.hide();
+            button.hide();
+
+            var name= input.value();
+            playerCount+= 1;
+            player.update(name);
+            player.updateCount(playerCount);
+
+            greeting.html("Hello "+name);
+            greeting.position(130,160);
+        });
+    }
+}
